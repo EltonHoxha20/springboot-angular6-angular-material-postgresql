79e4fc6a1283ced791a1c8f0b1c7d03b6c6e912f
Lucky Pratama <luckypratama71@yahoo.com>
2018-06-14 09:45:04 +0700

    Updating customer-angular app

--- customer-angular/src/app/app.module.ts
+++ customer-angular/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CustomersComponent } from './customers/customers.component';
 import { CustomerDetailComponent } from './customer-detail/customer-detail.component';
 import { AppRoutingModule } from './/app-routing.module';
-
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +14,9 @@ import { AppRoutingModule } from './/app-routing.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- customer-angular/src/app/customer.service.ts
+++ customer-angular/src/app/customer.service.ts
@@ -1,11 +1,23 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Customer } from './customer';
+
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
 
-  constructor() { }
+  private heroesUrl = 'http://localhost:8090/customers';
+
+  getCustomers(): Observable<Customer[]> {
+    return this.http.get<Customer[]>(this.heroesUrl);
+  }
+  constructor(
+    private http: HttpClient
+  ) { }
 }
